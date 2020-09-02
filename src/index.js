8b3f6b4d5917d5ad46f79719eb2fea29f2d258fb
Rosannavdt <68822156+Rosannavdt@users.noreply.github.com>
2020-09-02 13:49:57 +0200

    Added the Search engine

--- src/index.js
+++ src/index.js
@@ -20,10 +20,22 @@ function displayTemperature(response) {
   icon.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "92a130a46565e7d85d7d55d01afb2d4a";
-let city = "New York";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(displayTemperature);
+function search(city) {
+  let apiKey = "92a130a46565e7d85d7d55d01afb2d4a";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function doSubmitting(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#input");
+  search(cityInput.value);
+}
+
+search("New York");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", doSubmitting);
 
 let days = [
   "Sunday",
