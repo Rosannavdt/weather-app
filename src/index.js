d41a4e9ef5256a4dadf7ad8d49fff3bf0188df76
Rosannavdt <68822156+Rosannavdt@users.noreply.github.com>
2020-09-02 13:24:46 +0200

    API integration
    Temperature
    Weather description
    City
    Date
    Windspeed and Humidity

---
+++ src/index.js
@@ -0,0 +1,67 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let city = document.querySelector("#city");
+  let description = document.querySelector("#description");
+  let humidity = document.querySelector("#humidity");
+  let wind = document.querySelector("#wind");
+
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  city.innerHTML = response.data.name;
+  description.innerHTML = response.data.weather[0].description;
+  humidity.innerHTML = response.data.main.humidity;
+  wind.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "92a130a46565e7d85d7d55d01afb2d4a";
+let city = "New York";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayTemperature);
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+let newDate = new Date();
+let currentHour = newDate.getHours();
+if (currentHour < 10) {
+  currentHour = `0${currentHour}`;
+}
+let currentMinute = newDate.getMinutes();
+if (currentMinute < 10) {
+  currentMinute = `0${currentMinute}`;
+}
+let currentDay = days[newDate.getDay()];
+let currentMonth = months[newDate.getMonth()];
+let currentDate = newDate.getDate();
+let currentYear = newDate.getFullYear();
+
+let time = document.querySelector("#time");
+let day = document.querySelector("#day");
+
+let date = document.querySelector("#date");
+
+time.innerHTML = `${currentHour} : ${currentMinute} `;
+day.innerHTML = `${currentDay}`;
+date.innerHTML = `${currentMonth}, ${currentDate} , ${currentYear}`;
